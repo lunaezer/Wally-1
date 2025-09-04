17dca56b4bfc9ccee9a12b292bc734dbc1cd657c
lunaezer <lunaezer22@gmail.com>
2025-09-04 11:39:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import { generarEspacio } from "./busqueda.js";
 
-let lista_nombre = generarEspacio(100)
+let listanombre = generarEspacio(100)
 
 for(let i = 0; i<100; i++){
-    if(lista_nombre[i] == "Wally"){
-        console.log("Wally esta en" + i)
+    if(listanombre[i] == "Wally"){
+        console.log("Wally esta en " + i, listanombre[i])
     }
 }
+console.log("\nNombres que empiezan con W:");
+for (let i = 0; i < listanombre.length; i++) {
+  if (listanombre[i][0] === "W") {
+    console.log(`Posición ${i}: ${listanombre[i]}`);
+  }
+}
+
+console.log("\nNombres que contienen 'll':");
+for (let i = 0; i < listanombre.length; i++) {
+  if (listanombre[i].includes("ll")) {
+    console.log(`Posición ${i}: ${listanombre[i]}`);
+  }
+}
+
+
